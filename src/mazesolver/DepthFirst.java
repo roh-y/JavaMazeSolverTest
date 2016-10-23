b99be16a477c46831dfad81b6e6a6db7465be2ee
Leo <ono.leo@gmail.com>
2016-10-23 18:37:46 -0200

    Update DepthFirst.java
    removed comments

--- src/mazesolver/DepthFirst.java
+++ src/mazesolver/DepthFirst.java
@@ -16,23 +16,15 @@ public class DepthFirst {
     public static boolean searchPath(int[][] maze, int x, int y
             , List<Integer> path) {
         
-        // finally, let's check if the target node was reached
         if (maze[y][x] == 9) {
             path.add(x);
             path.add(y);
             return true;
-            // ok i think that's it :)
         }
         
-        // when the current position (x and y) is
-        // a not-visited node (0), then let's
-        // mark it as visited (2)
         if (maze[y][x] == 0) {
             maze[y][x] = 2;
             
-            // now, let's visit all neighbor nodes recursively.
-            // If path was found, let's fill the path list
-            // with current position
             int dx = -1;
             int dy = 0;
             if (searchPath(maze, x + dx, y + dy, path)) {
